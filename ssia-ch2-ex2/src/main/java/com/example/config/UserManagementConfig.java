790bd59a7a1a06e83476f96eea1703fd2020595b
danniel.kil <danniel.kil@dannielkils-MacBook-Air.local>
2024-10-01 16:50:16 +0900

    로그 추가 및 ProjectConfig 전체주석

--- ssia-ch2-ex2/src/main/java/com/example/config/UserManagementConfig.java
+++ ssia-ch2-ex2/src/main/java/com/example/config/UserManagementConfig.java
@@ -1,16 +1,20 @@
 package com.example.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@Configuration
 public class UserManagementConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
+        System.out.println("UserManagementConfig");
+
         var userDetailsService = new InMemoryUserDetailsManager();
 
         var user = User.withUsername("john")
--- ssia-ch2-ex2/src/main/java/com/example/config/WebAuthorizationConfig.java
+++ ssia-ch2-ex2/src/main/java/com/example/config/WebAuthorizationConfig.java
@@ -1,12 +1,19 @@
 package com.example.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class WebAuthorizationConfig extends WebSecurityConfigurerAdapter  {
+    // ProjectConfig, WebAuthorizationConfig 두개의 config 클래스에서
+    // WebSecurityConfigurerAdapter를 확장하면 Exception이 발생하기 때문에
+    // ProjectConfig은 전체 주석처리
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        System.out.println("WebAuthorizationConfig");
+
         http.httpBasic();
         http.authorizeRequests()
                 .anyRequest().authenticated(); // 모든 요청에 인증 요구
