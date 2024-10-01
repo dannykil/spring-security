e921d446b87a564abb4432e2f7ec6ad62878da55
danniel.kil <danniel.kil@dannielkils-MacBook-Air.local>
2024-10-01 14:18:40 +0900

    Exception 관련 주석 추가

--- ssia-ch2-ex2/src/main/java/com/example/config/ProjectConfig.java
+++ ssia-ch2-ex2/src/main/java/com/example/config/ProjectConfig.java
@@ -26,6 +26,9 @@ public class ProjectConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
+        // 기본 UserDetailsService를 이용하면 PasswordEncoder도 자동 구성되지만
+        // UserDetailsService를 재정의하면 PasswordEncoder도 다시 선언해야함
+        // 선언하지 않는 경우 There is no PasswordEncoder mapped for the id "null" 이라는 Exception 발생
         return NoOpPasswordEncoder.getInstance();
     }
 }
