e5f8eccfde43f6d6395783c9607dd79d080a5926
danniel.kil <danniel.kil@dannielkils-MacBook-Air.local>
2024-10-03 13:44:12 +0900

    ex03 - LdapUserDetailsManager

--- ssia-ch3-ex1/src/main/java/com/example/config/ProjectConfig.java
+++ ssia-ch3-ex1/src/main/java/com/example/config/ProjectConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.ldap.DefaultLdapUsernameToDnMapper;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
@@ -36,20 +39,43 @@ import java.util.List;
 //}
 
 // ex02 - JdbcUserDetailsManager
+//@Configuration
+//public class ProjectConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        String usersByUsernameQuery = "select username, password, enabled from spring.users where username = ?";
+//        String authsByUserQuery = "select username, authority from spring.authorities where username = ?";
+//
+//        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+//
+//        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+//        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+//
+//        return userDetailsManager;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+//}
+
+// ex03 - LdapUserDetailsManager
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
     @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        String usersByUsernameQuery = "select username, password, enabled from spring.users where username = ?";
-        String authsByUserQuery = "select username, authority from spring.authorities where username = ?";
+    public UserDetailsService userDetailsService() {
+        var cs = new DefaultSpringSecurityContextSource("ldap://127.0.0.1:33389/dc=springframework,dc=org");
+        cs.afterPropertiesSet();
 
-        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
-
-        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
-        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
-
-        return userDetailsManager;
+        LdapUserDetailsManager manager = new LdapUserDetailsManager(cs);
+        manager.setUsernameMapper(
+                new DefaultLdapUsernameToDnMapper("ou=groups", "uid"));
+        manager.setGroupSearchBase("ou=groups");
+        return manager;
     }
 
     @Bean
