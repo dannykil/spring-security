a170b697bce008497b296fadb9c1b8b04e837a59
danniel.kil <danniel.kil@dannielkils-MacBook-Air.local>
2024-10-03 12:35:50 +0900

    JdbcUserDetailsManager

--- ssia-ch3-ex1/src/main/java/com/example/config/ProjectConfig.java
+++ ssia-ch3-ex1/src/main/java/com/example/config/ProjectConfig.java
@@ -4,29 +4,57 @@ import com.example.model.User;
 import com.example.service.InMemoryUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
+import javax.sql.DataSource;
 import java.util.List;
 
-// @Configuration를 삽입하지 않았더니 시큐리티 기본 설정만 적용되고 해당 파일의 설정은 완전히 무시됨
+// ex01
+// @Configuration를 삽입하지 않았더니 시큐리티 기본 설정만 적용되고 아래 설정은 적용되지 않음
+//@Configuration
+//public class ProjectConfig {
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        System.out.println("ProjectConfig");
+//        UserDetails user = new User("john", "12345", "read");
+//
+//        List<UserDetails> users = List.of(user);
+//        return new InMemoryUserDetailsService(users);
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+//}
+
+// ex02
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        System.out.println("ProjectConfig");
-        UserDetails user = new User("john", "12345", "read");
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery = "select username, password, enabled from spring.users where username = ?";
+        String authsByUserQuery = "select username, authority from spring.authorities where username = ?";
 
-        List<UserDetails> users = List.of(user);
-        return new InMemoryUserDetailsService(users);
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+
+        return userDetailsManager;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
+
 }
